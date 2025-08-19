b5485c4835c5b49b5f526ca6b8695c6f64fae542
Venkatesh200510 <venkateshsgounskar@gmail.com>
2025-08-19 22:12:37 +0530

    Initial Commit

---
+++ dist/script.js
@@ -0,0 +1,33 @@
+const inputTodo=document.getElementById("input-todo");
+const addTodo=document.getElementById("add-todo");
+const todoContainer=document.querySelector(".container");
+
+addTodo.addEventListener("click",e=> {
+    if(inputTodo.value != ""){
+        displayTodo(inputTodo.value);
+        inputTodo.value="";
+    }
+})
+
+function displayTodo(todoValue){
+    let todoWrapper=document.createElement("div");
+    todoWrapper.classList.add("todo-wrapper");
+
+    todoWrapper.innerHTML=`
+    <div class="todo bg-white  text-2xl flex items-center justify-between mx-8 my-2 px-2 py-2 rounded mt-10">
+                <p>${inputTodo.value}</p>
+                <div class="actions">
+                    <input type="checkbox" class="mr-4">
+                    <button class="delete-btn bg-red-600 text-white px-2 py-1 rounded">Delete</button>
+                </div>
+            </div>
+    `;
+
+    let deleteButton=todoWrapper.querySelector(".delete-btn");
+    deleteButton.addEventListener("click",function deleteTodo(){
+        todoWrapper.remove();
+    });
+
+
+    todoContainer.appendChild(todoWrapper);
+}
